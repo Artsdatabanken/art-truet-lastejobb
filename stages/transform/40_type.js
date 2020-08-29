b7251017f8db1f1911bf89732771455e39a4b485
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2020-08-29 19:36:16 +0200

    lastejobb  ^3.5.3  →  ^3.7.1

--- stages/transform/40_type.js
+++ stages/transform/40_type.js
@@ -1,6 +1,7 @@
 const { io, url } = require("lastejobb");
 
 let tre = io.lesTempJson("art-truet-ubehandlet/type.json");
-new url(tre).assignUrls();
-
+tre["RL"].foreldre = []
+tre["RL"].tittel = { nb: 'Truet art/natur' }
+new url(tre).assignUrls()
 io.skrivBuildfil("type", tre);
